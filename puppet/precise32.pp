5abb1d8ed51f94b485e6412dfcbd74cf8c88bd51
Steven Emmerson <steve@unidata.ucar.edu>
2013-11-19 10:32:02 -0700

    Removed "update" and dependencies on it

--- puppet/precise32.pp
+++ puppet/precise32.pp
@@ -4,37 +4,29 @@ group { "puppet":
 
 File { owner => 0, group => 0, mode => 0644 }
 Exec { path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'], }
-  
-exec {'update': command => 'apt-get update', }
 
 package {'make':
   ensure  => present,
-  require => Exec['update'],
 }
 
 package {'libcunit1':
   ensure  => present,
-  require => Exec['update'],
 }
 
 package {'libcunit1-dev':
   ensure  => present,
-  require => Exec['update'],
 }
 
 package {'cmake':
   ensure  => present,
-  require => Exec['update'],
 }
 
 package {'expat':
   ensure  => present,
-  require => Exec['update'],
 }
 
 package {'libexpat-dev':
   ensure  => present,
-  require => Exec['update'],
 }
 
 exec {'bashrc':
