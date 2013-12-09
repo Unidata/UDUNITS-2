591c378d4888bc55b5e2feb237379bfeee7259ae
steve <steve@unidata.ucar.edu>
2013-12-09 15:26:34 -0700

    Added release number

--- push.sh
+++ push.sh
@@ -1,14 +1,16 @@
 # This script does the following:
 #     1. Ensures correct version information in the CMake configuration-file
 #        and the texinfo(1) version file;
-#     2. Commits to the local repository if appropriate;
-#     3. Tags the HEAD revision with the current version; and
-#     4. Pushes to the remote repository
+#     2. Updates the release number.
+#     3. Commits to the local repository if appropriate;
+#     4. Tags the HEAD revision with the current version; and
+#     5. Pushes to the remote repository
+#     6. Updates the package version of the previous release.
 
-set -e  # exit if error
+set -e  # exit upon error
 
 #
-# Set the package version in the CMake configuration-file.
+# Get the package version information from the CHANGE_LOG file.
 #
 versionId=`awk '{print $1;exit}' CHANGE_LOG`
 majorId=`echo $versionId | cut -d . -f 1`
@@ -19,6 +21,9 @@ test "$majorId"
 test "$minorId"
 test "$patchId"
 
+#
+# Set the package version in the CMake configuration-file.
+#
 sed "
 /^[ \t]*[Ss][Ee][Tt][ \t]*(VERSION_MAJOR[ \t]/cSET(VERSION_MAJOR $majorId)
 /^[ \t]*[Ss][Ee][Tt][ \t]*(VERSION_MINOR[ \t]/cSET(VERSION_MINOR $minorId)
@@ -36,8 +41,30 @@ sed "
 mv version.texi.tmp version.texi
 
 #
+# If the current package version differs from that of the previous release
+# (i.e., the previous invocation of this script),
+#
+if ! test $versionId = `cat previousVersionId`; then
+    #
+    # A new package version is being released. Reset the release number.
+    #
+    echo 1 >releaseNo
+elif
+    #
+    # The same package version is being released. Increment the release number.
+    #
+    releaseNo=$((`cat releaseNo` + 1))
+    echo $releaseNo >releaseNo
+fi
+
+#
 # Commit, tag, and push the package.
 #
 git commit -a || true
 git tag -f v$versionId
 git push
+
+#
+# Set the package version of the prevous release.
+#
+echo $versionId >previousVersionId
