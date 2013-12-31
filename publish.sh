08161cc90e505cc9a1ca65d7796a8d4b560e90c5
steve <steve@unidata.ucar.edu>
2013-12-31 13:03:20 -0700

    Merge binary yum(1) repository actions into "upload" script

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@
 # upstream job that creates a binary distribution.
 #
 # Usage:
-#     $0 pipeId nJobs binDistroFile srcDistroFile binRepoRelDir docDistroFile binDistroGlob pkgName indexHtml
+#     $0 pipeId nJobs binDistroFile srcDistroFile binRepoRelDir docDistroFile pkgName indexHtml
 #
 # where:
 #     pipeId                Unique identifier for the parent delivery pipeline
@@ -18,8 +18,6 @@
 #     binRepoRelDir         Pathname of the binary repository directory relative to
 #                           the root of the binary repository
 #     docDistroFile         Pathname of the documentation distribution file
-#     binDistroGlob         Glob pattern matching filenames of all releases of
-#                           this version of the binary distribution of the package
 #     pkgName               Name of the package (e.g., "udunits")
 #     indexHtml             Name of the top-level HTML documentation file (e.g.,
 #                           "udunits2.html")
@@ -30,9 +28,8 @@ binDistroFile=${3:?Binary distribution file not specified}
 srcDistroFile=${4:?Source distribution file not specified}
 binRepoRelDir=${5:?Relative pathname of binary repository directory not specified}
 docDistroFile=${6:?Documentation distribution file not specified}
-binDistroGlob=${7:?Release-independent, version-dependent filename glob pattern not specified}
-pkgName=${8:?Package name not specified}
-indexHtml=${9:?Top-level HTML documentation-file not specified}
+pkgName=${7:?Package name not specified}
+indexHtml=${8:?Top-level HTML documentation-file not specified}
 
 binRepoHost=spock                # Name of computer hosting binary repository
 binRepoRoot=repo                 # Pathname of the root directory of the binary
@@ -45,39 +42,33 @@ webHost=webserver                # Name of computer hosting package website
 
 set -e  # exit on failure
 
-#
 # Indicates if the outcome of the upstream jobs is decidable.
 #
 decidable() {
     test `ls $jobId.success $jobId.failure 2>/dev/null | wc -w` -ge $nJobs
 }
 
-#
 # Indicates if the upstream jobs were successful.
 #
 success() {
     test `ls $jobId.success 2>/dev/null | wc -w` -ge $nJobs
 }
 
-#
 # Ensure valid pathnames.
 #
 binDistroFile=`ls $binDistroFile`
 srcDistroFile=`ls $srcDistroFile`
 
 
-#
 # Form a unique identifier for this invocation.
 #
 binDistroFileName=`basename $binDistroFile`
 jobId=$pipeId-$binDistroFileName
 
-#
 # Remove any leftovers from an earlier delivery pipeline.
 #
 ls *.success *.failure 2>/dev/null | grep -v ^$jobId | xargs rm -rf
 
-#
 # Make known to all invocations of this script in the delivery pipeline the
 # outcome of the upstream job associated with this invocation.
 #
@@ -87,32 +78,22 @@ else
     touch $jobId.failure
 fi
 
-#
 # Wait until the outcome of all the upstream jobs can be decided.
 #
 while ! decidable; do
     sleep 3
 done
 
+# Upload the binary distribution to the binary repository.
 #
-# Copy the binary distribution to the binary repository.
-#
-binRepoDir=$binRepoRoot/$binRepoRelDir
-trap "ssh $binRepoHost rm -f $binRepoDir/$binDistroFileName; `trap -p ERR`" ERR
-success && scp $binDistroFile $binRepoHost:$binRepoDir
+success && 
+    ssh -T $binRepoHost $binRepoRoot/upload $binRepoRelDir/$binDistroFileName \
+        <$binDistroFile
 
+# Set the absolute path to the public source distribution file.
 #
-# Delete all previous binary releases of the same package version.
-#
-ssh $binRepoHost "ls $binRepoDir/$binDistroGlob | fgrep -v $binDistroFileName | xargs rm -f"
-
-#
-# Rebuild the binary repository.
-#
-ssh $binRepoHost $binRepoRoot/rebuild $binRepoRelDir
-
 srcDistroPath=/web/ftp/pub/$pkgName/`basename $srcDistroFile`
-#
+
 # If the source repository doesn't have the source distribution,
 #
 if ! ssh $srcRepoHost test -e $srcDistroPath; then
@@ -123,15 +104,16 @@ if ! ssh $srcRepoHost test -e $srcDistroPath; then
     scp $srcDistroFile $srcRepoHost:$srcDistroPath
 fi
 
+# Set some variables for managing the package's website.
+#
 pkgId=`basename $docDistroFile | sed 's/^\([^-]*-[0-9.]*\).*/\1/'`
 version=`echo $pkgId | sed 's/^[^-]*-//'`
 pkgWebDir=/web/content/software/$pkgName
 versionWebDir=$pkgWebDir/$pkgId
-#
+
 # If the package's website doesn't have this version's documentation,
 #
 if ! ssh $webHost test -e $versionWebDir; then
-    #        
     # Provision the package's website with the documentation distribution. NB:
     # Assumes that the first component of all pathnames in the distribution is
     # "share/".
@@ -141,12 +123,12 @@ if ! ssh $webHost test -e $versionWebDir; then
         ssh $webHost "cd $pkgWebDir && pax -r -s ';share/;$pkgId/;'"
     ssh $webHost "cd $versionWebDir && rm -f index.html && ln -s doc/$pkgName/${indexHtml} index.html"
     ssh $webHost "cd $versionWebDir && cp doc/$pkgName/CHANGE_LOG $pkgWebDir"
-    #
+
     # Ensure that the top-level HTML file contains a reference to this version.
     #
     ssh -T $webHost <<EOF
         cd $pkgWebDir
-        #
+
         # Ensure that the top-level HTML file contains a reference to this
         # version.
         #
@@ -159,7 +141,7 @@ if ! ssh $webHost test -e $versionWebDir; then
         ' index.html >index.html.new
         cp index.html index.html.old
         mv index.html.new index.html
-        #
+
         # Set the curent-version symbolic link to the current version
         #
         rm -f $pkgName-current
