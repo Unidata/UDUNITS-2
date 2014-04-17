cf3e0ae13c682cfda18820ac8bae9b6ccc0d546f
steve <steve@unidata.ucar.edu>
2014-04-17 13:32:17 -0600

    Release 2.2.9

--- release-vars.sh
+++ release-vars.sh
@@ -2,7 +2,7 @@
 
 # Base definitions:
 export PKG_NAME=udunits
-export PKG_VERSION=2.2.8
+export PKG_VERSION=2.2.9
 export SOURCE_DISTRO_EXT=tar.gz
 export ABSPATH_FTP_PUB_DIR=/web/ftp/pub
 export SOURCE_REPO_HOST=ftpserver
