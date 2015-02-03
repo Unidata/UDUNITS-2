caba6020b4f456b9ff4c742139adf1ca88c19dd0
steve <steve@unidata.ucar.edu>
2015-02-03 15:33:35 -0700

    First stage in "delivery/" creation

--- acceptance_test_linux.sh
+++ acceptance_test_linux.sh
@@ -1,10 +1,14 @@
 # Performs an acceptance-test of a package on a Linux system. Creates a binary
-# distribution file and a documentation distribution file. The current directory
-# must contain the source-distribution file and the release-variables file.
+# distribution file and a documentation distribution file. The directory that
+# contains this script must also contain the files
+#     - release-vars.sh
+#     - Vagrantfile
+#     - repo_add
 #
-# Usage: $0 vmName vmCpu generator ext install [binRepoDir]
+# Usage: $0 tarball vmName vmCpu generator ext install [binRepoDir]
 #
 # where:
+#     tarball           Pathname of the source distribution file.
 #     vmName            Name of the Vagrant virtual machine (e.g.,
 #                       "centos64_64", "precise32")
 #     vmCpu             Type of VM CPU (e.g., "x86_64")
@@ -25,34 +29,42 @@ set -e # terminate on error
 
 # Parse the command-line.
 #
-VM_NAME=${1:?Name of Vagrant virtual-machine not specified}
-VM_CPU=${2:?Virtual-machine CPU not specified}
-GENERATOR=${3:?Name of CPack package-generator not specified}
-EXT=${4:?Package extension not specified}
-INSTALL=${5:?Installation command not specified}
-binRepoDir=${6:?Platform-specific binary-repository directory not specified}
+tarball=${1:?Pathname of tarball not specified}
+vmName=${2:?Name of Vagrant virtual-machine not specified}
+vmCpu=${3:?Virtual-machine CPU not specified}
+generator=${4:?Name of CPack package-generator not specified}
+ext=${5:?Package extension not specified}
+install=${6:?Installation command not specified}
+binRepoDir=${7:?Platform-specific binary-repository directory not specified}
 
-# Get the static release variables.
+binDistroName="$PKG_ID.$vmCpu"
+binDistroFilename=$binDistroName.$ext
+
+# Set the build directory to the directory that contains this script.
+builddir=`dirname $0`
+
+# Move the tarball to the build directory so that it appears in the "/vagrant"
+# directory.
 #
+mv $tarball $builddir
+
+# Make the build directory the current working directory.
+#
+cd $builddir
+
+# Set the release-variables.
 . ./release-vars.sh
-
-binDistroName="$PKG_ID.$VM_CPU"
-binDistroFilename=$binDistroName.$EXT
-
-# Make the directory that contains this script be the current working directory.
-#
-cd `dirname $0`
 
 # Start the virtual machine. Ensure that each virtual machine is started
 # separately because vagrant(1) doesn't support concurrent "vagrant up" 
-# invocations.
+# invocations in the same directory.
 #
-trap "vagrant destroy --force $VM_NAME; `trap -p EXIT`" EXIT
-flock "$SOURCE_DISTRO_NAME" -c "vagrant up \"$VM_NAME\""
+trap "vagrant destroy --force $vmName; `trap -p EXIT`" EXIT
+flock "$SOURCE_DISTRO_NAME" -c "vagrant up \"$vmName\""
 
 # On the virtual machine,
 #
-vagrant ssh $VM_NAME -- -T <<EOF
+vagrant ssh $vmName -- -T <<EOF
     set -e # terminate on error
 
     # Unpack the source distribution.
@@ -68,7 +80,7 @@ vagrant ssh $VM_NAME -- -T <<EOF
     # and create a binary distribution.
     #
     cmake -DCMAKE_INSTALL_PREFIX=$ABSPATH_DEFAULT_INSTALL_PREFIX \
-        -DCPACK_PACKAGE_FILE_NAME=$binDistroName -DCPACK_GENERATOR=$GENERATOR
+        -DCPACK_PACKAGE_FILE_NAME=$binDistroName -DCPACK_GENERATOR=$generator
     make all test
     sudo make install install_test package
 
@@ -81,21 +93,25 @@ EOF
 #
 # Restart the virtual machine.
 #
-vagrant destroy --force $VM_NAME
-flock "$SOURCE_DISTRO_NAME" -c "vagrant up \"$VM_NAME\""
+vagrant destroy --force $vmName
+flock "$SOURCE_DISTRO_NAME" -c "vagrant up \"$vmName\""
 
 # On the virtual machine,
 #
-vagrant ssh $VM_NAME -- -T <<EOF
+vagrant ssh $vmName -- -T <<EOF
     set -e # terminate on error
 
     # Verify that the package installs correctly from the binary distribution.
     #
-    sudo $INSTALL /vagrant/$binDistroFilename
+    sudo $install /vagrant/$binDistroFilename
     $ABSPATH_DEFAULT_INSTALL_PREFIX/bin/udunits2 -A -H km -W m
 
     # Add the binary-distribution to the yum(1) binary-repository.
     #
-    bash -x /vagrant/repo_add /repo $binRepoDir/$VM_CPU \
+    bash -x /vagrant/repo_add /repo $binRepoDir/$vmCpu \
             /vagrant/$binDistroFilename
 EOF
+
+# Move the binary distribution to the workspace.
+#
+mv $binDistroFilename ../..
