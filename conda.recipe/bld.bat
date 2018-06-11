1c972f2083e5965d2960108473c0c5e17d08c536
Filipe Fernandes <ocefpaf@gmail.com>
2018-06-11 12:15:06 -0300

    fix appveyor build

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -10,6 +10,8 @@ copy .\lib\*.xml %LIBRARY_PREFIX%\share\udunits\ || exit 1
 
 copy %RECIPE_DIR%\unistd.h %SRC_DIR% || exit 1
 
+mkdir build
+cd build
 cmake -G "NMake Makefiles" ^
       -D EXPAT_INCLUDE_DIR=%LIBRARY_INC%\expat.h ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -20,7 +22,9 @@ if errorlevel 1 exit 1
 nmake libudunits2 || exit 1
 nmake udunits2 || exit 1
 
+
 copy prog\udunits2.exe %SCRIPTS%\udunits2.exe || exit 1
 copy lib\udunits2.dll %SCRIPTS%\udunits2.dll || exit 1
+copy ..\lib\*.h %LIBRARY_INC%\ || exit 1
 copy lib\*.exp %LIBRARY_LIB%\ || exit 1
 copy lib\*.lib %LIBRARY_LIB%\ || exit 1
